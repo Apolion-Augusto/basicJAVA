ab4c21d13aba806ebbbcba7cf73da00c80f694e2
Apolion Augusto E S. F. Melo <155980502+Apolion-Augusto@users.noreply.github.com>
2024-12-03 12:06:05 +0100

    DecimalToBinary

---
+++ src/basicJAVA/DecimalToBinary.java
@@ -0,0 +1,13 @@
+package basicJAVA;
+
+public class DecimalToBinary {
+
+    public static void main(String[] args) {
+
+        int num = 10;
+
+        String binary = Integer.toBinaryString(num);
+
+        System.out.println(binary);
+    }
+}
