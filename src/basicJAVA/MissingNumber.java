0b90f521f10977fabd1ecdbf73ad952c94a2d517
Apolion Augusto E S. F. Melo <155980502+Apolion-Augusto@users.noreply.github.com>
2024-12-03 12:05:06 +0100

    MissingNumber

---
+++ src/basicJAVA/MissingNumber.java
@@ -0,0 +1,21 @@
+package basicJAVA;
+
+public class MissingNumber {
+
+    public static void main(String[] args) {
+
+        int [] arr = {1, 2, 3, 5, 6};
+
+        int n = arr.length + 1;
+
+        int total = n * (n + 1) / 2;
+
+        for(int num : arr){
+
+            total -= num;
+
+        }
+
+        System.out.println(total);
+    }
+}
